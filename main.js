746f78dd56420af042d787792409ba750cb9f38d
asiansouldja <60935812+asiansouldja@users.noreply.github.com>
2021-05-16 18:32:05 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+$('.menu__burger').on('click', function(){
+$('.menu__burger, .menu').toggleClass('active');
+$('body').toggleClass('lock');
+});
+
+$('.menu__list-link').on('click', function(){
+  $('.menu__burger, .menu').removeClass('active');
+  $('body').removeClass('lock');
+  });
+
+
+
+
